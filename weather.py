b0ea0438afe87483a51ad5ffeb46a6251064d3e1
InfiniteLoop <65031541+LoopBloop@users.noreply.github.com>
2020-06-06 16:22:14 +0600

    Initial commit

---
+++ weather.py
@@ -0,0 +1,22 @@
+import requests
+
+##Made By Sabbir Hossain
+print(".................Made By Sabbir Hossain.................")
+city=input('Enter your city name:')
+url='http://api.openweathermap.org/data/2.5/weather?q={}&appid=975154a1d4adacf218847495eead535e&units=metric'.format(city)
+res=requests.get(url)
+data=res.json()
+temp=data['main']['temp']
+wind=data['wind']['speed']
+des=data['weather'][0]['description']
+
+print("Todays Temperature:",temp)
+print("Wind Speed:",wind)
+print("Weather description:",des)
+
+et=int(input("Enter 1 to exit:"))
+if et==1:
+    exit()
+else:
+    print("Enter the correct number")
+
